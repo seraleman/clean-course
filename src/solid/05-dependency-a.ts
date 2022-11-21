1239c9440393b8068f182a5047536a0792155c1e
Sergio Alejandro Manrique <sergio.manrique@multienlace.com.co>
2022-11-21 10:14:44 -0500

    Principio de inversión de dependencia

---
+++ src/solid/05-dependency-a.ts
@@ -0,0 +1,18 @@
+import { PostService } from './05-dependency-b'
+import {
+  JsonDataBaseService,
+  LocalDataBaseService,
+  webApiService,
+} from './05-dependency-c'
+
+// Main
+;(async () => {
+  // const provider = new JsonDataBaseService()
+  // const provider = new LocalDataBaseService()
+  const provider = new webApiService()
+  const postService = new PostService(provider)
+
+  const posts = await postService.getPosts()
+
+  console.log({ posts })
+})()
